2883d515a766d14848d9085092f54e8d8e87bf3d
punoko <36707250+punoko@users.noreply.github.com>
2023-09-17 06:17:36 +0200

    first commit

---
+++ config.h
@@ -0,0 +1,4 @@
+#include "config_common.h"
+
+// #undef  FORCE_NKRO
+#define FORCE_NKRO
---
+++ rules.mk
@@ -0,0 +1,12 @@
+# Overwriting defaults from dz60/rules.mk
+
+# MCU = atmega32u4
+# BOOTLOADER = atmel-dfu
+
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = no
+BACKLIGHT_ENABLE = no
+
+RGBLIGHT_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+NKRO_ENABLE = yes
